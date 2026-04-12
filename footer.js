2e7bdf23e5a4cfa5616385c5e33102895d88d311
tmiya3 <is0283ir@gmail.com>
2026-04-12 15:27:09 +0900

    フッタを修正

--- footer.js
+++ footer.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 	console.log(location.pathname)
- 	// 現在のページのパスを取得
- 	const depth = location.pathname.split("/").length - 2;
-
+	// 現在のページのパスを取得
+	const depth = location.pathname.split("/").length - 2;
+	console.log(depth)
  	// 階層に応じて ../ を生成
  	const prefix = "../".repeat(depth);
 
 	console.log(prefix)
 	// footer.html を読み込む
-	fetch("../footer.html")
+	fetch(prefix+"footer.html")
 		.then(res => res.text())
 		.then(html => {
 		document.getElementById("footer").innerHTML = html;
